47a43e703d5d80d3b20a9c9fdcf1c362453f07d6
Maximiliano Gomez <alexander34223@gmail.com>
2024-01-24 17:06:47 -0300

    estructura de tp 5 de react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'
+import Footer from './components/Footer';
+
+const App=()=> {
+  return (
+    <>
+
+    <main>
+      <h2 className='text-center py-5'>Lista Tarea Con LocalStorage Ejercicio 5 :</h2>
+
+    </main>
+
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Footer = () => {
+    return (
+        <div className='text-center py-4 bg-black text-light'>
+            <p>&copy;derechos reservados</p>
+        </div>
+    );
+};
+
+export default Footer;
